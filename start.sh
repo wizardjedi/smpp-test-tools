d6ca1abf5373d1c2bad66ee1408faf3bdac76720
Sayapin Alexander <wizardjedi@gmail.com>
2013-06-12 22:07:27 +0400

    * Fixed start shell script

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -jar target/smpptester-1.0.jar -wait -h 127.0.0.1:2775 -u smppclient1 -p password -tc -rebindperiod 30 -elinkperiod 2 -speed 1
+java -jar target/smpptester-1.0.jar -h 127.0.0.1:2775 -u smppclient1 -p password -tc 1:1:791111234567 5:0:test ucs-2:'привет земляне!!!' 8
