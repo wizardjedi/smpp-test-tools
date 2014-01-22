82b189a9ddb92a900f339e8b1093f42f9531ca5c
Sayapin Alexander <a.sayapin@alt1.ru>
2014-01-22 17:03:21 +0400

    + All clients will use the same scheduled thread pool. Added jackson for json serialization.

--- http-adapter/src/main/java/com/a1systems/http/adapter/servlet/StatusServlet.java
+++ http-adapter/src/main/java/com/a1systems/http/adapter/servlet/StatusServlet.java
@@ -1,7 +1,10 @@
 package com.a1systems.http.adapter.servlet;
 
 import com.a1systems.http.adapter.Application;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import java.io.IOException;
+import java.util.ArrayList;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -12,11 +15,17 @@ public class StatusServlet extends HttpServlet {
     @Autowired
     protected Application application;
 
+    @Autowired
+    protected ObjectMapper objectMapper;
+    
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
         response.setStatus(HttpServletResponse.SC_OK);
-        response.getWriter().println("Status"+application.getMessageParts().keySet().toString()+application.getLinkIds().toString()+application.getMessages().keySet().toString());
+        
+        String data = objectMapper.writeValueAsString(new ArrayList<String>());
+        
+        response.getWriter().println(data);
     }
 
 }
--- http-adapter/src/main/java/com/a1systems/http/adapter/smpp/client/Client.java
+++ http-adapter/src/main/java/com/a1systems/http/adapter/smpp/client/Client.java
@@ -16,137 +16,135 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Client {
-	public static final Logger log = LoggerFactory.getLogger(Client.class);
 
-	protected SmppSessionConfiguration cfg;
-	protected SmppSessionHandler sessionHandler;
-	protected ClientState state;
+    public static final Logger log = LoggerFactory.getLogger(Client.class);
 
-	protected volatile SmppSession session;
+    protected SmppSessionConfiguration cfg;
+    protected SmppSessionHandler sessionHandler;
+    protected ClientState state;
 
-	protected SmppClient smppClient;
+    protected volatile SmppSession session;
 
-	protected ScheduledExecutorService timer;
+    protected SmppClient smppClient;
 
-	protected ScheduledFuture<?> elinkTask;
-	protected ScheduledFuture<?> rebindTask;
+    protected ScheduledExecutorService timer;
 
-	protected long rebindPeriod = 5;
-	protected long elinkPeriod = 5;
+    protected ScheduledFuture<?> elinkTask;
+    protected ScheduledFuture<?> rebindTask;
+
+    protected long rebindPeriod = 5;
+    protected long elinkPeriod = 5;
 
     protected int speed = 30;
     protected RateLimiter rateLimiter;
 
     protected DelayQueue<MessagePart> queue;
 
-	public Client(SmppSessionConfiguration cfg) {
-		this.cfg = cfg;
+    public Client(SmppSessionConfiguration cfg) {
+        this.cfg = cfg;
 
         this.state = ClientState.IDLE;
 
-		this.timer = Executors.newScheduledThreadPool(2);
-
         this.queue = new DelayQueue<MessagePart>();
-	}
+    }
 
-	public void start() {
-		log.debug("Starting client");
+    public void start() {
+        log.debug("Starting client");
 
-		this.smppClient = new DefaultSmppClient();
+        this.smppClient = new DefaultSmppClient();
 
         this.rateLimiter = RateLimiter.create(this.speed);
 
-		this.bind();
-	}
+        if (this.timer == null) {
+            this.timer = Executors.newScheduledThreadPool(2);
+        }
+        
+        this.bind();
+    }
 
     public MessagePart poll() {
         return queue.poll();
     }
 
-	private void runRebindTask() {
-		this.rebindTask = this.timer.scheduleAtFixedRate(new RebindTask(this), 0, getRebindPeriod(), TimeUnit.SECONDS);
-	}
+    private void runRebindTask() {
+        this.rebindTask = this.timer.scheduleAtFixedRate(new RebindTask(this), 0, getRebindPeriod(), TimeUnit.SECONDS);
+    }
 
-	private void runElinkTask() {
-		this.elinkTask = this.timer.scheduleAtFixedRate(new ElinkTask(this), getElinkPeriod(), getElinkPeriod(), TimeUnit.SECONDS);
-	}
+    private void runElinkTask() {
+        this.elinkTask = this.timer.scheduleAtFixedRate(new ElinkTask(this), getElinkPeriod(), getElinkPeriod(), TimeUnit.SECONDS);
+    }
 
+    public void bind() {
+        if (this.state == ClientState.BOUND
+                || this.state == ClientState.IDLE) {
+            log.debug("Binding state");
 
-	public void bind() {
-		if (
-			this.state == ClientState.BOUND
-			|| this.state == ClientState.IDLE
-		) {
-			log.debug("Binding state");
+            if (this.session != null
+                    && this.session.isBound()) {
+                this.session.close();
+                this.session.destroy();
+            }
 
-			if (
-				this.session != null
-				&& this.session.isBound()
-			) {
-				this.session.close();
-				this.session.destroy();
-			}
+            this.state = ClientState.BINDING;
 
-			this.state = ClientState.BINDING;
+            if (elinkTask != null) {
+                this.elinkTask.cancel(true);
+            }
+            runRebindTask();
+        }
+    }
 
-			if (elinkTask != null) {
-				this.elinkTask.cancel(true);
-			}
-			runRebindTask();
-		}
-	}
+    public void bound(SmppSession session) {
+        if (this.state == ClientState.BINDING) {
+            log.debug("Bound state");
 
-	public void bound(SmppSession session) {
-		if (this.state == ClientState.BINDING) {
-			log.debug("Bound state");
+            this.state = ClientState.BOUND;
 
-			this.state = ClientState.BOUND;
+            this.session = session;
 
-			this.session = session;
+            if (rebindTask != null) {
+                this.rebindTask.cancel(true);
+            }
+            runElinkTask();
+        }
+    }
 
-			if (rebindTask!=null) {
-				this.rebindTask.cancel(true);
-			}
-			runElinkTask();
-		}
-	}
+    public void stop() {
+        log.debug("Stopping");
 
-	public void stop() {
-		log.debug("Stopping");
+        this.state = ClientState.STOPPING;
 
-		this.state = ClientState.STOPPING;
+        this.elinkTask.cancel(true);
+        this.rebindTask.cancel(true);
+        this.timer.shutdown();
 
-		this.elinkTask.cancel(true);
-		this.rebindTask.cancel(true);
-		this.timer.shutdown();
+        this.timer = null;
+    }
 
-		this.timer = null;
-	}
+    // getters and setters
+    public long getRebindPeriod() {
+        return rebindPeriod;
+    }
 
-	// getters and setters
-	public long getRebindPeriod() {
-		return rebindPeriod;
-	}
+    public void setRebindPeriod(long rebindPeriod) {
+        this.rebindPeriod = rebindPeriod;
+    }
 
-	public void setRebindPeriod(long rebindPeriod) {
-		this.rebindPeriod = rebindPeriod;
-	}
+    public long getElinkPeriod() {
+        return elinkPeriod;
+    }
 
-	public long getElinkPeriod() {
-		return elinkPeriod;
-	}
+    public void setElinkPeriod(long elinkPeriod) {
+        this.elinkPeriod = elinkPeriod;
+    }
 
-	public void setElinkPeriod(long elinkPeriod) {
-		this.elinkPeriod = elinkPeriod;
-	}
+    public SmppClient getSmppClient() {
+        return smppClient;
+    }
 
-	public SmppClient getSmppClient() {
-		return smppClient;
-	}
-
-	public void setSmppClient(SmppClient smppClient) {
-		this.smppClient = smppClient;
-	}
+    public void setSmppClient(SmppClient smppClient) {
+        this.smppClient = smppClient;
+    }
 
     public int getSpeed() {
         return speed;
@@ -164,33 +162,40 @@ public class Client {
         this.rateLimiter = rateLimiter;
     }
 
-	public SmppSessionConfiguration getCfg() {
-		return cfg;
-	}
+    public SmppSessionConfiguration getCfg() {
+        return cfg;
+    }
 
-	public void setCfg(SmppSessionConfiguration cfg) {
-		this.cfg = cfg;
-	}
+    public void setCfg(SmppSessionConfiguration cfg) {
+        this.cfg = cfg;
+    }
 
-	public SmppSessionHandler getSessionHandler() {
-		return sessionHandler;
-	}
+    public SmppSessionHandler getSessionHandler() {
+        return sessionHandler;
+    }
 
-	public void setSessionHandler(SmppSessionHandler sessionHandler) {
-		this.sessionHandler = sessionHandler;
-	}
+    public void setSessionHandler(SmppSessionHandler sessionHandler) {
+        this.sessionHandler = sessionHandler;
+    }
 
-	public SmppSession getSession() {
-		return session;
-	}
+    public SmppSession getSession() {
+        return session;
+    }
 
-	public void setSession(SmppSession session) {
-		this.session = session;
-	}
+    public void setSession(SmppSession session) {
+        this.session = session;
+    }
+
+    public ScheduledExecutorService getTimer() {
+        return timer;
+    }
+
+    public void setTimer(ScheduledExecutorService timer) {
+        this.timer = timer;
+    }
 
     public void addToQueue(MessagePart part) {
         this.queue.add(part);
     }
-
 
 }
