a8da6385981d51a50fceba22140e02582dd800d9
Sayapin Alexander <wizardjedi@gmail.com>
2014-05-19 00:50:12 +0400

    + Added tick task

---
+++ smpp-simulator/src/main/java/com/a1systems/smpp/simulator/TickTask.java
@@ -0,0 +1,33 @@
+package com.a1systems.smpp.simulator;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.script.Invocable;
+import javax.script.ScriptException;
+
+class TickTask implements Runnable {
+
+    protected Application app;
+    protected SimulatorSession simSession;
+
+    public TickTask(Application app, SimulatorSession simSession) {
+        this.app = app;
+        this.simSession = simSession;
+    }
+
+    @Override
+    public void run() {
+        if (app.getInvocableEngine()!=null) {
+            Invocable invocableEngine = app.getInvocableEngine();
+
+            try {
+                invocableEngine.invokeFunction(ScriptConstants.HANDLER_ON_TICK, simSession);
+            } catch (ScriptException ex) {
+                Logger.getLogger(TickTask.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (NoSuchMethodException ex) {
+                Logger.getLogger(TickTask.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+
+}
