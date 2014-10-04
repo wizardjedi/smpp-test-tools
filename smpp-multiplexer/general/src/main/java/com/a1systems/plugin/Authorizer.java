ef35bdb3ea37689185966dfae940cf5658f5c8a4
Sayapin Alexander <wizardjedi@gmail.com>
2014-10-04 17:53:38 +0400

    + Added javadoc for interfaces

--- smpp-multiplexer/general/src/main/java/com/a1systems/plugin/Authorizer.java
+++ smpp-multiplexer/general/src/main/java/com/a1systems/plugin/Authorizer.java
@@ -1,5 +1,18 @@
 package com.a1systems.plugin;
 
+/**
+ * This interface contains methods for authorization mechanism.
+ */
 public interface Authorizer extends Plugin {
+    /**
+     * Authorize session with systemid and password. SMPP-mux try to auth
+     * sessions with all registered authorizers. If authorizer will return
+     * {@code}true{@code} then loop will be terminated and access will granted
+     * to session.
+     *
+     * @param systemId
+     * @param password
+     * @return authoriztion status
+     */
     public boolean auth(String systemId, String password);
 }
--- smpp-multiplexer/general/src/main/java/com/a1systems/plugin/Plugin.java
+++ smpp-multiplexer/general/src/main/java/com/a1systems/plugin/Plugin.java
@@ -1,11 +1,30 @@
 package com.a1systems.plugin;
 
+/**
+ * Base plugin interface. All plugins have to implement this interface.
+ */
 public interface Plugin {
+    /**
+     * This method will be invoked on loading plugin.
+     */
     public void load();
 
+    /**
+     * This method will be invoked on starting plugin. For some plugins
+     * {@code}start(){@code} method can be invoke just after {@code}load(){@code}
+     * method.
+     */
     public void start();
 
+
+    /**
+     * This method will be invoked on stopping plugin.
+     */
     public void stop();
 
+    /**
+     * This method have to return plugin name and version.
+     * @return plugin name and version
+     */
     public String getDescription();
 }
