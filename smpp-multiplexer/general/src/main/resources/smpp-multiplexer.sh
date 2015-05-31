b9ebce9d8cb50d2cfee3a4ff4425445abf9d64aa
Sayapin Alexander <wizardjedi@gmail.com>
2015-05-31 14:11:42 +0300

    + Added comments and plugins directory to class path.

--- smpp-multiplexer/general/src/main/resources/smpp-multiplexer.sh
+++ smpp-multiplexer/general/src/main/resources/smpp-multiplexer.sh
@@ -2,11 +2,16 @@
 
 cd /usr/share/smpp-multiplexer/
 
+# Archive for GC-logs
 FILE="log/gc.$(date +%Y%m%d%H%M%S).tar.gz"
+
+#GC logs to process
 FILES_TO_PROCESS="log/gc.log.* log/error.log"
 
+#Archive GC logs
 `/bin/tar -czf $FILE $FILES_TO_PROCESS`
 
-java -Xms2048m -Xmx2048m -Xloggc:log/gc.log -verbose:gc -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintGCTimeStamps -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=5 -XX:GCLogFileSize=128K -jar -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=9011 -Dcom.sun.management.jmxremote.authenticate=false -Djava.rmi.server.hostname=`/bin/hostname -i` -Dcom.sun.management.jmxremote.ssl=false -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/usr/share/smpp-multiplexer/log/ -Dlogback.configurationFile=etc/logback.xml ${project.build.finalName}.jar "$@" &> log/error.log &
+#Java run command
+java -cp plugins/* -Xms2048m -Xmx2048m -Xloggc:log/gc.log -verbose:gc -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintGCTimeStamps -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=5 -XX:GCLogFileSize=128K -jar -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=9011 -Dcom.sun.management.jmxremote.authenticate=false -Djava.rmi.server.hostname=`/bin/hostname -i` -Dcom.sun.management.jmxremote.ssl=false -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/usr/share/smpp-multiplexer/log/ -Dlogback.configurationFile=etc/logback.xml ${project.build.finalName}.jar "$@" &> log/error.log &
 
 echo $! > /usr/share/smpp-multiplexer/run.pid
